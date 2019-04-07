574f7b6b41b9647675edaf6fa3827e8125c72070
Abdourahmane Diop <cabraldiop18@gmail.com>
2019-04-07 23:25:31 +0000

    design page daccueil

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,11 +11,30 @@
 
 <h4>Nos Articles</h4>
 @if (! $posts->isEmpty())
-    <ul>
+    <div class="row">
         @foreach ($posts as $post)
+        <div class="col-4 mb-4">
+            <div class="border border-dark rounded p-2">
+                <div class="card-header">
+                   <h3> {{ $post->title }} </h3>
+                </div>
+                <div class="card-text">
+                   <p> {{ $post->body }} </p>
+                </div>
+                <a class="btn btn-primary" href="{{ route('blog.show',$post->slug) }}">Lire la suite</a>
+                <a href="" class="btn btn-link">
+                    <i class="far fa-thumbs-up"></i>
+                    <span class="js-likes">10</span>
+                </a>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    {{-- <ul>
+         @foreach ($posts as $post)
             <li><a href="{{ route('blog.show',$post->slug) }}">{{ $post->title }}</a></li>
         @endforeach
-    </ul>
+    </ul> --}}
     {{ $posts->links() }}
 @else
     <p>Aucun article</p>
